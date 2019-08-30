d93aeeef81f838a271a8a73edb624734045ce449
杨小康(10029913) <yangxk-a@glodon.com>
2019-08-30 11:14:54 +0800

    flag学习

--- day09/05argv/main.go
+++ day09/05argv/main.go
@@ -2,20 +2,23 @@
  * @Author: yangxiaokang
  * @Date: 2019-08-29 17:22:16
  * @Last Modified by: yangxiaokang
- * @Last Modified time: 2019-08-29 18:40:41
+ * @Last Modified time: 2019-08-30 09:51:31
  */
 
 package main
+
 import (
 	"fmt"
 	"os"
 )
 
-func main(){
-	if len(os.Args) >  0 {
-		for index,arg := range os.Args {
-			fmt.Printf("args[%d]=%v\n",index,arg)
+func main() {
+
+	fmt.Println(os.Args)                            //如同shell的$*
+	fmt.Println(os.Args[0], os.Args[1], os.Args[2]) //如同shell $1 $2
+	if len(os.Args) > 0 {
+		for index, arg := range os.Args {
+			fmt.Printf("序号%v 值%v \n", index, arg)  //os.args本质是字符串切片
 		}
 	}
-
 }
--- day09/06flag/main.go
+++ day09/06flag/main.go
@@ -7,13 +7,28 @@ import (
 )
 
 func main()  {
-	name := flag.String("name","lucy","请输入名字")
-	age := flag.Int("age",20,"请输入年龄")
-	married := flag.Bool("married",false,"请输入年龄")
-	cTime := flag.Duration("ct",time.Second,"结婚多久")
+	// name := flag.String("name","lucy","请输入名字")
+	// age := flag.Int("age",20,"请输入年龄")
+	// married := flag.Bool("married",false,"请输入年龄")
+	// cTime := flag.Duration("ct",time.Second,"结婚多久")
+	// flag.Parse()
+	// fmt.Println(*name)
+	// fmt.Println(*age)
+	// fmt.Println(*married)
+	// fmt.Println(*cTime)dir
+
+	var name string
+	var age int
+	var married bool
+	var delay time.Duration
+	
+	flag.StringVar(&name,"name","lucy","姓名")
+	flag.IntVar(&age,"age",20,"年龄")
+	flag.BoolVar(&married,"married",false,"婚否")
+	flag.DurationVar(&delay,"d",0,"时间间隔")
 	flag.Parse()
-	fmt.Println(*name)
-	fmt.Println(*age)
-	fmt.Println(*married)
-	fmt.Println(*cTime)
+	fmt.Println(name,age,married,delay)
+	fmt.Println(flag.Args())
+	fmt.Println(flag.NArg())
+	fmt.Println(flag.NFlag())
 }
