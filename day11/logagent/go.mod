8cf70de8712dc3bb7c2e0df6134b0f9a84c9fd84
zicheqingluo <841429973@qq.com>
2019-09-15 17:35:02 +0800

    day11项目

--- day11/logagent/go.mod
+++ day11/logagent/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/Shopify/sarama v1.23.1
 	github.com/hpcloud/tail v1.0.0
 	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/ini.v1 v1.46.0
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
--- day11/logagent/main.go
+++ day11/logagent/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"studygo/day11/logagent/conf"
 	"fmt"
 	"studygo/day11/logagent/kafka"
 	"studygo/day11/logagent/taillog"
 	"time"
+	"gopkg.in/ini.v1"
+)
+
+var (
+	cfg = new(conf.AppConf)
 )
 
 func run() {
@@ -13,7 +19,8 @@ func run() {
 	for {
 		select {
 		case line := <-taillog.ReadChan():
-			kafka.SendToKafka("web_log", line.Text)
+			//kafka.SendToKafka("web_log", line.Text)
+			kafka.SendToKafka(cfg.KafkaConf.Topic, line.Text)
 		default:
 			time.Sleep(time.Second)
 		}
@@ -22,15 +29,30 @@ func run() {
 }
 
 func main() {
+	//0.加载配置文件
+	// cfg,err := ini.Load("./conf/config.ini")
+	// addr := cfg.Section("kafka").Key("address").String()
+	// topic := cfg.Section("topic").Key("topic").String()
+	// path := cfg.Section("taillog").Key("path").String()
+	// fmt.Println(addr)
+	err := ini.MapTo(cfg,"./conf/config.ini")
+	if err != nil {
+		fmt.Println("init kafka failed",err)
+		return
+	}
+
 	//1.初始化kafka链接
-	err := kafka.Init([]string{"127.0.0.1:9092"})
+	//err = kafka.Init([]string{"127.0.0.1:9092"})
+	err = kafka.Init([]string{cfg.KafkaConf.Address})
+	fmt.Println("地址",cfg.KafkaConf.Address,cfg.KafkaConf.Topic,cfg.TaillogConf.FileName)
 	if err != nil {
 		fmt.Printf("init kafka failed,err:%v \n", err)
 		return
 	}
 	fmt.Println("init kafka secuss...")
 	//2.打开日志文件准备收集日志
-	err = taillog.Init("./my.log")
+	//err = taillog.Init("./my.log")
+	err = taillog.Init(cfg.TaillogConf.FileName)
 	if err != nil {
 		fmt.Printf("Init taillog failed,err:%v \n", err)
 	}
