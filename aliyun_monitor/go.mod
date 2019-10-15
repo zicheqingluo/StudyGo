ff3468510f80823c301909f292af182a0c053f6a
zicheqingluo <841429973@qq.com>
2019-10-15 23:33:30 +0800

    sdddd

--- aliyun_monitor/go.mod
+++ aliyun_monitor/go.mod
@@ -2,4 +2,4 @@ module StudyGo/aliyun_monitor
 
 go 1.13
 
-require github.com/aliyun/alibaba-cloud-sdk-go v0.0.0-20191014065940-8899d95cb5f0 // indirect
+require github.com/aliyun/alibaba-cloud-sdk-go v0.0.0-20191014065940-8899d95cb5f0
--- aliyun_monitor/main.go
+++ aliyun_monitor/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
   	"github.com/aliyun/alibaba-cloud-sdk-go/services/cms"
   
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
+
+
   
 )
 
@@ -23,23 +26,79 @@ func main() {
 	if err != nil {
 		fmt.Print(err.Error())
 	}
-	fmt.Printf("response is %v\n", response)
+	fmt.Println(response)
+	
+	
+	type ContactSmses struct{
+		ContactSms []interface{}  `json:"ContactSms"`
+	}
+	type ContactMails struct {
+		ContactMail []interface{}`json:"ContactMail"`
+	}	
 
-	type AlarmHistory struct {
-
+	type Contacts   struct {
+		Contact []interface{} `json:"Contact"`
 	}
 
-	type AlarmHistoryList struct {
-		AlarmHistory	AlarmHistory
-
+	type ContactALIIMs struct {
+		ContactALIIM []interface{} `json:"ContactALIIM"`
 	}
 
-	type  res_data struct {
-		AlarmHistoryList AlarmHistoryList 
-		RequestID	string
-		Success	bool
-		Code	string 
-		Total	int 
+	type ContactGroups struct {
+		ContactGroup []interface{} `json:"ContactGroup"`
 	}
+
+	
+
+	type AlarmHistory []struct {
+		Value        string `json:"Value"`
+		LastTime     int    `json:"LastTime"`
+		Webhooks     string `json:"Webhooks"`
+		ContactSmses ContactSmses `json:"ContactSmses"` 
+		RuleName        string `json:"RuleName"`
+		GroupID         string `json:"GroupId"`
+		AlertName       string `json:"AlertName"`
+		EvaluationCount int    `json:"EvaluationCount"`
+		Status          int    `json:"Status"`
+		AlertState      string `json:"AlertState"`
+		MetricName      string `json:"MetricName"`
+		ContactMails ContactMails `json:"ContactMails"`
+		AlertTime  int64  `json:"AlertTime"`
+		Dimensions string `json:"Dimensions"`
+		RuleID     string `json:"RuleId"`
+		Contacts Contacts `json:"Contacts"`
+		Namespace     string `json:"Namespace"`
+		ContactALIIMs ContactALIIMs  `json:"ContactALIIMs"`
+		ContactGroups ContactGroups `json:"ContactGroups"`
+		Expression   string `json:"Expression"`
+		Level        string `json:"Level"`
+		InstanceName string `json:"InstanceName"`
+	}
+
+    type AlarmHistoryList struct {
+		AlarmHistory	AlarmHistory `json:"AlarmHistory"`
+
+	} 
+
+	type resData struct {
+		AlarmHistoryList	AlarmHistoryList	`json:"AlarmHistoryList"`
+		RequestID string `json:"RequestId"`
+		Success   bool   `json:"Success"`
+		Code      string `json:"Code"`
+		Total     int    `json:"Total"`
+	}
+
+
+
+	var Data resData
+	res := fmt.Sprintf(`%v`,response)
+	fmt.Println(res)
+	res1 := `{"AlarmHistoryList":{"AlarmHistory":[{"Value":"100","LastTime":1037048011,"Webhooks":"","ContactSmses":{"ContactSms":[]},"RuleName":"diskusage_utilization","GroupId":"14103","AlertName":"applyTemplate371eca68-5217-4655-95e7-084649c2a852","EvaluationCount":3,"Status":2,"AlertState":"ALARM","MetricName":"diskusage_utilization#60","ContactMails":{"ContactMail":[]},"AlertTime":1569824888000,"Dimensions":"{\"device\":\"/dev/vdb1\",\"instanceId\":\"i-25u9dm3xi\"}","RuleId":"","Contacts":{"Contact":[]},"Namespace":"acs_ecs","ContactALIIMs":{"ContactALIIM":[]},"ContactGroups":{"ContactGroup":[]},"Expression":"$Average>=90","Level":"P2","InstanceName":""}]},"RequestId":"42827853-C3F7-411F-88BA-BABFF6770467","Success":true,"Code":"200","Total":2039}`
+	err = json.Unmarshal([]byte(res1),&Data)
+	if err != nil {
+		fmt.Println("json unmarshal err:",err)
+	}
+	fmt.Println(Data.Total)
+
 
 }
